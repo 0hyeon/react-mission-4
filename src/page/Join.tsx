baa38403d16f11edf2ab8e0bea185f90274655c4
0hyeon <djdjdjk2006@naver.com>
2023-02-23 12:47:14 +0900

    feat:build setting

--- src/page/Join.tsx
+++ src/page/Join.tsx
@@ -15,7 +15,7 @@ function Join() {
       password: isPassword,
     }
     axios
-      .post(process.env.MOCK_SERVER_URL + `/register`, body, {
+      .post(process.env.REACT_APP_MOCK_SERVER_URL + `/register`, body, {
         withCredentials: true,
       })
       .then((res) => {
